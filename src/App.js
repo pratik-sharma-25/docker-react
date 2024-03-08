e450b58a5cc4a181ab35bc71ac1aafa744425421
Pratik Sharma <pratik@Pratiks-MacBook-Pro.local>
2024-03-08 17:55:38 +0530

    cjecking deployment

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hi there pratik sharmassss
+          Hi there pratik sharma
         </p>
         <a
           className="App-link"
